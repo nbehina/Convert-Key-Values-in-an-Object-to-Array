ada17a206581d6a5d99ce03765429191894a9de0
Your Namenbehina <nbehina@gmail.com>
2020-04-02 22:59:36 -0600

    Convert Key, Values in an Object to Array

---
+++ main.js
@@ -0,0 +1,7 @@
+var myArray = {
+    D: 1,
+    B: 2,
+    C: 3
+};
+console.log(myArray);
+//# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,8 @@
+
+const myArray: Record<string, number> = { 
+  D: 1,
+  B: 2,
+  C: 3
+}
+
+console.log(myArray); 
